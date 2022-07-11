443bfa3a60df6c8fab4bc853c3b9ed180d3f03c7
William Philipp <william_philipp@web.de>
2022-07-11 14:46:07 +0200

    adapted model to problem

--- model.py
+++ model.py
@@ -7,23 +7,23 @@ class ConvNet (nn.Module):
         super().__init__()
 
         self.encoder = nn.Sequential(
-            nn.Conv2d(in_channels=24, out_channels=32, kernel_size=5, padding=2, stride = 2, bias=False),
-            nn.BatchNorm2d(num_features=32),
+            nn.Conv1d(in_channels=11, out_channels=32, kernel_size=5, padding=2, stride = 2, bias=False),
+            nn.BatchNorm1d(num_features=32),
             nn.ReLU(),
             #nn.MaxPool1d(2),
 
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, padding=2, stride = 2, bias=False),
-            nn.BatchNorm2d(num_features=64),
+            nn.Conv1d(in_channels=32, out_channels=64, kernel_size=5, padding=2, stride = 2, bias=False),
+            nn.BatchNorm1d(num_features=64),
             nn.ReLU(),
             #nn.MaxPool1d(2),
 
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=5, padding=2, stride = 2, bias=False),
-            nn.BatchNorm2d(num_features=128),
+            nn.Conv1d(in_channels=64, out_channels=128, kernel_size=5, padding=2, stride = 2, bias=False),
+            nn.BatchNorm1d(num_features=128),
             nn.ReLU(),
             #nn.MaxPool1d(2)
         )
 
-        self.pool = nn.AdaptiveAvgPool2d(output_size=1)
+        self.pool = nn.AdaptiveAvgPool1d(output_size=1)
         self.classifier = nn.Sequential(
             nn.Flatten(),
             nn.Linear(in_features=128, out_features=55)
@@ -38,4 +38,4 @@ class ConvNet (nn.Module):
 
 
 if __name__ == '__main__':
-    summary(ConvNet(), (24, 200))
+    summary(ConvNet(), (11, 300))
